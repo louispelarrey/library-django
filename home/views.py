d8fd18afe161f1f46b579af126ae4476cd5f6e32
calintha <cal.intha77@gmail.com>
2022-11-26 15:46:49 +0100

    feat: create home dir and update gitignore

---
+++ home/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_index(request):
+    return HttpResponse("Hello, world. You're at the home index.")
--- library/urls.py
+++ library/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("home.urls")),
     path('admin/', admin.site.urls),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
