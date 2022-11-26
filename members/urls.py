7c043c47836fbcd75983bfed8324e150d300a850
louispelarrey <louispelarrey@gmail.com>
2022-11-26 15:21:37 +0100

    register implemented

--- members/urls.py
+++ members/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'members'
+
 urlpatterns = [
     path('login_user/', views.login_user, name='login'),
+    path('logout_user/', views.logout_user, name='logout'),
+    path('register_user/', views.register_user, name='register'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_user(request):
     if request.method == 'POST':
@@ -14,3 +15,18 @@ def login_user(request):
             messages.error(request, 'Le nom de compte ou le mot de passe est incorrect')
     context = {}
     return render(request, 'authenticate/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('members:login')
+
+def register_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return redirect('members:login')
+    context = {}
+    return render(request, 'authenticate/register.html', context)
