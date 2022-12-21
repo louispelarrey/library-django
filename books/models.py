41d8880e0e512b8a4f50014effe479d7cbf84da5
calintha <cal.intha77@gmail.com>
2022-12-21 19:55:10 +0100

    fix: import model

--- books/models.py
+++ books/models.py
@@ -1,52 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.template.defaultfilters import slugify
+from libraries.models import Library
 
 # Create your models here.
 User = get_user_model()
-
-class Book(models.Model):
-    title = models.CharField(max_length=255, unique=True, verbose_name='Titre')
-    slug = models.SlugField(max_length=255, unique=True, blank=True)
-    description = models.TextField(verbose_name='Description')
-    image = models.ImageField(upload_to='images', verbose_name='Image')
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Auteur')
-    editor = models.ForeignKey(Editor, on_delete=models.CASCADE, verbose_name='Editeur')
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, verbose_name='Collection')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Catégorie')
-
-    class Meta:
-        verbose_name = 'Livre'
-        verbose_name_plural = 'Livres'
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return self.title
-
-    def save(self, *args, **kwargs):
-
-        self.slug = slugify(self.title)
-
-        super().save(*args, **kwargs)
-
-class Overdue(models.Model):
-    reference = models.CharField(max_length=255, verbose_name='Référence')
-    loan_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, default='En cours', verbose_name='Statut de l\'emprunt')
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'Emprunt'
-        verbose_name_plural = 'Emprunts'
-        ordering = ['-date']
-
-    def __str__(self):
-        return self.book.title
 
 class Author(models.Model):
     name = models.CharField(max_length=255, unique=True, verbose_name='Nom de l\'auteur')
@@ -123,3 +81,44 @@ class Category(models.Model):
         self.slug = slugify(self.name)
 
         super().save(*args, **kwargs)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name='Titre')
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    description = models.TextField(verbose_name='Description')
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Auteur')
+    editor = models.ForeignKey(Editor, on_delete=models.CASCADE, verbose_name='Editeur')
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, verbose_name='Collection')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Catégorie')
+
+    class Meta:
+        verbose_name = 'Livre'
+        verbose_name_plural = 'Livres'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+
+        self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+class Overdue(models.Model):
+    reference = models.CharField(max_length=255, verbose_name='Référence')
+    loan_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, default='En cours', verbose_name='Statut de l\'emprunt')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Emprunt'
+        verbose_name_plural = 'Emprunts'
+
+    def __str__(self):
+        return self.book.title
