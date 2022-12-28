b8aefb32e4c827e519fec8032b3927c1ceb6960e
calintha <cal.intha77@gmail.com>
2022-12-28 13:55:47 +0100

    feat: create books view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import views
+from books.models import Book, Author, Collection, Editor, Overdue, Category
 
-# Create your views here.
+def book_index(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'book/index.html', context)
+
